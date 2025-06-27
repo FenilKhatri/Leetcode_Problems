ed5e755ff21b178d478832daea09707e2c0412c0
FenilKhatri <Fenilkhatri931@gmail.com>
2025-06-27 20:10:58 +0530

    add

---
+++ Problem - 0000 to 0100/Problem - 27 copy/js-solution.js
@@ -0,0 +1,10 @@
+let nums = [0,1,2,2,3,0,4,2], val = 2, count = 0;
+
+for(let i=0; i<nums.length; i++) {
+    if(nums[i] == val) {
+        nums[i] = '_';
+        count++;
+    }
+}
+
+console.log(nums, (nums.length - count));
